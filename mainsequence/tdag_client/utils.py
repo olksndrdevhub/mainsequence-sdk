3ddaaca6f633c5c8ab876f790da527bb13db8a22
daniel.gaerber <daniel.gaerber@main-sequence.io>
2025-02-05 16:48:28 +0100

    debug

--- mainsequence/tdag_client/utils.py
+++ mainsequence/tdag_client/utils.py
@@ -169,6 +169,8 @@ def make_request(s, r_type: str, url: str, loaders: Union[AuthLoaders, None], pa
                  ):
     from requests.models import Response
 
+    logger.debug(f"Requesting {r_type} from {url}")
+
     TIMEOFF = .25
     TRIES = 15 // TIMEOFF
     payload = {} if payload is None else payload
