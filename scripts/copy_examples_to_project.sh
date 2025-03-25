2afb7e70922e6dd3f84b8596df4c014611192898
daniel.gaerber <daniel.gaerber@main-sequence.io>
2025-03-25 13:43:18 +0100

    system tests

--- scripts/copy_examples_to_project.sh
+++ scripts/copy_examples_to_project.sh
@@ -33,7 +33,8 @@ cd "$ROOT_PROJECT_PATH" || true
 pull_changes
 
 # Copy examples
-cp -a "/opt/code/mainsequence-sdk/examples/time_series" "$VFB_PROJECT_PATH/scripts" || echo "WARNING: Copy TimeSeries step failed!"
+cp -a "/opt/code/mainsequence-sdk/examples/time_series" "$VFB_PROJECT_PATH/time_series" || echo "WARNING: Copy TimeSeries step failed!"
+cp -a "/opt/code/mainsequence-sdk/tests/system_tests" "$VFB_PROJECT_PATH/scripts" || echo "WARNING: Copy System Tests step failed!"
 
 echo "Copy examples commit"
 cd "$ROOT_PROJECT_PATH"
