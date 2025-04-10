ecf6e9341d79a6e7fbb76219741a91db40ee1d78
daniel.gaerber <daniel.gaerber@main-sequence.io>
2025-04-10 11:15:58 +0200

    hf

---
+++ examples/apps/run_named_portfolio.py
@@ -0,0 +1,30 @@
+from mainsequence.virtualfundbuilder.models import PortfolioConfiguration
+from mainsequence.virtualfundbuilder.utils import get_vfb_logger
+
+from pydantic import BaseModel
+from mainsequence.client.models_tdag import Artifact
+from mainsequence.virtualfundbuilder.resource_factory.app_factory import BaseApp, register_app
+
+logger = get_vfb_logger()
+
+class NamedPortfolioConfiguration(BaseModel):
+    portfolio_name: str = "market_cap_example"
+    update_tree: bool = True
+
+@register_app()
+class RunNamedPortfolio(BaseApp):
+    configuration_class = NamedPortfolioConfiguration
+
+    def __init__(self, configuration: NamedPortfolioConfiguration):
+        logger.info(f"Run Named Timeseries Configuration {configuration}")
+        self.configuration = configuration
+
+    def run(self) -> None:
+        from mainsequence.virtualfundbuilder.portfolio_interface import PortfolioInterface
+        portfolio = PortfolioInterface.load_from_configuration(self.configuration.portfolio_name)
+        res = portfolio.run(update_tree=self.configuration.update_tree)
+        logger.info(f"Portfolio Run successful with results {res.head()}")
+
+if __name__ == "__main__":
+    configuration = NamedPortfolioConfiguration()
+    RunNamedPortfolio(configuration).run()
--- examples/apps/run_portfolio.py
+++ examples/apps/run_portfolio.py
@@ -1,3 +1,5 @@
+from mainsequence.virtualfundbuilder.portfolio_interface import PortfolioInterface
+
 from mainsequence.virtualfundbuilder.models import PortfolioConfiguration
 from mainsequence.virtualfundbuilder.utils import get_vfb_logger
 
@@ -7,36 +9,19 @@ from mainsequence.virtualfundbuilder.resource_factory.app_factory import BaseApp
 
 logger = get_vfb_logger()
 
-@register_app(register_in_agent=False)
+@register_app()
 class RunPortfolio(BaseApp):
     configuration_class = PortfolioConfiguration
 
     def __init__(self, configuration: PortfolioConfiguration):
         self.configuration = configuration
-        pass
 
     def run(self) -> Artifact:
-        pass
-
-
-class NamedPortfolioConfiguration(BaseModel):
-    portfolio_name: str = "market_cap_example"
-    update_tree: bool = True
-
-@register_app()
-class RunNamedPortfolio(BaseApp):
-    configuration_class = NamedPortfolioConfiguration
-
-    def __init__(self, configuration: NamedPortfolioConfiguration):
-        logger.info(f"Run Named Timeseries Configuration {configuration}")
-        self.configuration = configuration
-
-    def run(self) -> None:
-        from mainsequence.virtualfundbuilder.portfolio_interface import PortfolioInterface
-        portfolio = PortfolioInterface.load_from_configuration(self.configuration.portfolio_name)
-        res = portfolio.run(update_tree=self.configuration.update_tree)
+        portfolio = PortfolioInterface(portfolio_config_template=self.configuration.model_dump())
+        res = portfolio.run()
         logger.info(f"Portfolio Run successful with results {res.head()}")
 
+
 if __name__ == "__main__":
-    configuration = NamedPortfolioConfiguration()
-    RunNamedPortfolio(configuration).run()
+    portfolio_configuration = PortfolioInterface.load_from_configuration("market_cap_example").portfolio_config
+    RunPortfolio(portfolio_configuration).run()
