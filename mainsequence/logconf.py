59b9758ed84b8249e28354ffce70aeb8297fef75
daniel.gaerber <daniel.gaerber@main-sequence.io>
2025-06-04 15:29:24 +0200

    add command id to logs

--- mainsequence/logconf.py
+++ mainsequence/logconf.py
@@ -190,6 +190,9 @@ def build_application_logger(
         logger = logger.bind(data_source_id=json_response["data_source_id"], **metadata)
         logger = logger.bind(job_run_id=json_response["job_run_id"], **metadata)
 
+        if command_id:
+            logger = logger.bind(command_id=command_id, **metadata)
+
     except Exception as e:
         logger.exception(f"Could not retrive pod project {e}")
         raise e
