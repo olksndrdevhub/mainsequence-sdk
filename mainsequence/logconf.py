9a6c28ee9b85301e989dc68437afbe30f8a52f9d
daniel.gaerber <daniel.gaerber@main-sequence.io>
2025-06-04 16:28:35 +0200

    command id fix

--- mainsequence/logconf.py
+++ mainsequence/logconf.py
@@ -189,7 +189,7 @@ def build_application_logger(
         logger = logger.bind(project_id=json_response["project_id"], **metadata)
         logger = logger.bind(data_source_id=json_response["data_source_id"], **metadata)
         logger = logger.bind(job_run_id=json_response["job_run_id"], **metadata)
-        logger = logger.bind(command_id=int(command_id), **metadata)
+        logger = logger.bind(command_id=int(command_id) if command_id else None, **metadata)
 
     except Exception as e:
         logger.exception(f"Could not retrive pod project {e}")
