3f27db5906788accf0e8dbced75a80f0a8f5ac2f
daniel.gaerber <daniel.gaerber@main-sequence.io>
2025-06-04 11:34:03 +0200

    fix requirements

--- scripts/get_git_and_run.sh
+++ scripts/get_git_and_run.sh
@@ -3,24 +3,24 @@ set -euo pipefail
 
 REPO_PATH="/tmp/repo"
 export VFB_PROJECT_PATH="$REPO_PATH/$PROJECT_LIBRARY_NAME"
-
-# check if we only execute a command on running
-if [ -n "${1:-}" ]; then
-  export COMMAND_ID="$1"
-  install_requirements_if_present "${REPO_PATH}/requirements.txt"
-  python -m mainsequence.virtualfundbuilder run_resource "app"
-  exit 0
-fi
-
+cd "$REPO_PATH"
 
 # Source our new utils
 source "$(dirname "$0")/utils.sh"
 
+# check if we only execute a command on running
+if [ -n "${1:-}" ]; then
+  export COMMAND_ID="$1"
+  # fix wrong python env set by shell execution TODO: set the correct path to the .bashrc
+  conda deactivate
+  source /opt/venv/bin/activate
+  python -m mainsequence.virtualfundbuilder run_resource "app"
+  exit 0
+fi
+
 # -------------------------------------------------------------
 # Decide whether to clone via SSH or API token
 # -------------------------------------------------------------
-
-
 if [ "${AUTHENTICATION_METHOD:-ssh}" = "api" ] && [ -n "${GIT_API_TOKEN:-}" ] && [ -n "${GIT_REPO_URL:-}" ]; then
   clone_via_api_token
 else
@@ -29,7 +29,6 @@ fi
 
 # -------------------------------------------------------------
 # Set necessary variables
-# (Note: the $REPO_PATH above is where we cloned the code)
 # -------------------------------------------------------------
 export TDAG_CONFIG_PATH=~/tdag/default_config.yml
 export TDAG_RAY_CLUSTER_ADDRESS="ray://localhost:10001"
@@ -38,7 +37,6 @@ export TDAG_RAY_SERVE_HOST="0.0.0.0"
 export TDAG_RAY_SERVE_PORT="8003"
 export MLFLOW_ENDPOINT="http://localhost:5000"
 
-cd "$REPO_PATH"
 export GIT_HASH="$(git rev-parse HEAD)"
 
 # Install requirements
