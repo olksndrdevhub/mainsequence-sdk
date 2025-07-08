4c5c95f4201ab7c20c70d960ed9b022657e65689
daniel.gaerber <daniel.gaerber@main-sequence.io>
2025-07-08 16:44:04 +0200

    cleanup

--- mainsequence/virtualfundbuilder/contrib/time_series/__init__.py
+++ mainsequence/virtualfundbuilder/contrib/time_series/__init__.py
@@ -2,5 +2,4 @@ from .intraday_trend import *
 from .market_cap import *
 from .mock_signal import *
 from .portfolio_replicator import *
-from .sentiment_signal import *
 from .external_weights import *
